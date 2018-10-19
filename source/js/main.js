a554adaf5455530d8496de9aaea5a4e1e075e98c
mirik49 <mirik49@yandex.ru>
2018-10-20 06:09:31 +0700

    На проверку

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,17 @@
 var btnOpen = document.querySelector(".main-nav__button-open");
-var mainNav = document.querySelector(".main-nav");
+var mainNav = document.querySelector(".main-nav__list");
+var mainShow = document.querySelector(".site-list")
 
 
 btnOpen.addEventListener("click", function(evt){
   if(btnOpen.classList.contains("main-nav__button-open--close")){
+    mainNav.classList.add("site-list");
     mainNav.classList.remove("main-nav-show");
     btnOpen.classList.remove("main-nav__button-open--close");
   }
   else {
     mainNav.classList.add("main-nav-show");
+    mainNav.classList.remove("site-list");
     btnOpen.classList.add("main-nav__button-open--close");
   }
 });
